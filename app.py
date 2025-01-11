65beb1aabd999f13d575a01056da7e6aab610ef8
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-11 09:45:38 +0530

    create to path and give path index,login,dshboard,rgister

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/')
+def Hello():
+    return render_template('index.html')
+
+@app.route('/register')
+def register1():
+    return render_template('index.html')
+
+@app.route('/login')
+def login1():
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard1(): 
+    return render_template('dashboard.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
